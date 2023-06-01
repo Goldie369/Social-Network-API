40390e1401a6d07bd3f15763bd9d249436346246
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-05-31 19:25:28 -0500

    adding a reaction to the models

---
+++ models/Reaction.js
@@ -0,0 +1,42 @@
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs');
+
+var advancedFormat = require('dayjs/plugin/advancedFormat');
+dayjs.extend(advancedFormat);
+
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: dateNow,
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+
+function dateNow (createdAt) {
+  return `${dayjs(createdAt).format('MMMM Do, YYYY')} at ${dayjs(createdAt).format('H:mm a')}`
+};
+
+module.exports = reactionSchema;
+  
