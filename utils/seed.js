84c838813c41c3811289250b6cc712e8a898f31c
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-06-01 21:05:57 -0500

    adding a commets to the seeds file for better understanding

--- utils/seed.js
+++ utils/seed.js
@@ -1,8 +1,21 @@
+// Adding a connection to a database using ../config/connection User and Thought models from ../models //
+// The connection variable likely imports the database connection configuration from ../config/connection //
+// The User and Thought variables imports the Mongoose models for the User and Thought schemas from ../models //
+// The username variable likely imports the array of user data from ./data //
+
+
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
 const { username } = require('./data');
 
 connection.on('error', (err) => err);
+// vent listener for the error event of the database connection //
+// Adding block listens for the open event of the database connection //
+// Adding message to the console indicating that the connection is established //
+// It deletes any existing data in the Thought and User collections using deleteMany() //
+// It seeds the database with the user data from the username array using insertMany() //
+// It logs the username array as a table in the console //
+// It logs a message indicating that the data is seeded //
 
 connection.once('open', async () => {
 
@@ -10,10 +23,10 @@ connection.once('open', async () => {
     await Thought.deleteMany({});
     await User.deleteMany({});
 
-    // Add users to the collection and await the results
+    
     await User.collection.insertMany(username);
     
-    // Log out seeded data
+    
     console.table(username);
     console.info('Data seeded.');
     process.exit(0);
