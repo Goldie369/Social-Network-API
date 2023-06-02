8537f63b4a6421888f1d4336c33c8d12cf5a43b1
Zlatelina Rizova <zlatelinarizova@gmail.com>
2023-06-01 20:29:55 -0500

    adding comments at server js for better understanding

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
+// Import necessary dependencies //
+
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// The PORT variable specifies the port on which the server will listen //
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// The express.urlencoded middleware parses incoming requests with URL //
+// It populates the req.body property with the parsed data //
+// Set up routes //
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// This code block listens for the open event of the db connection //
+// The callback function inside app.listen is executed when the server starts and logs a message indicating that the server is running //
+
 db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`App 🏃 on http://localhost:${PORT}`);
+      console.log(`App on http://localhost:${PORT}`);
     });
   });
   
